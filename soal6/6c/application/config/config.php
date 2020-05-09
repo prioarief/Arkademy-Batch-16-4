2ac98e69e289c3eed7847c818517bacae5cc3d47
prioarief <prioariefgunawangunawan@gmail.com>
2020-05-09 17:35:35 +0700

    finished

--- soal6/6c/application/config/config.php
+++ soal6/6c/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Tes-Arkademy/soal6/6c/';
+$config['base_url'] = 'http://localhost/Arkademy-Batch-16-4/soal6/6c/';
 
 /*
 |--------------------------------------------------------------------------
